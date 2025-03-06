15b7894a24870c29046d85bedc643ac3702d2821
Votre Nom <Vous@exemple.com>
2025-03-06 21:54:36 +0000

    Initial commit

---
+++ components/Spinner.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-5 text-center">
+    <b-spinner
+      variant="primary"
+      label="Chargement..."
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.spinner-border {
+  height: 100px;
+  width: 100px;
+}
+</style>
