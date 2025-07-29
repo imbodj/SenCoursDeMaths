34d083973ee187874d3915e82623ee272cc74d61
Hugo Delaunay <Skyost@users.noreply.github.com>
2025-07-29 23:13:12 +0200

    Ajout d'un titre.

--- pages/contact.vue
+++ pages/contact.vue
@@ -49,91 +49,94 @@ usePageHead({ title: 'Contact' })
 </script>
 
 <template>
-  <b-form
-    action="https://formspree.io/xwpqolpp"
-    :method="method"
-    @submit.prevent="onSubmitContactForm"
-  >
-    <b-form-row>
-      <b-col
-        md="6"
-        sm="12"
-        class="mb-3"
-      >
-        <label for="contact-name">
-          <icon name="bi:person-fill" /> Nom
-        </label>
-        <b-form-input
-          id="contact-name"
-          v-model="name"
-          :disabled="formLoading"
-          placeholder="Entrez votre nom"
-          aria-describedby="contact-name-help"
-          required
-        />
-      </b-col>
-      <b-col
-        md="6"
-        sm="12"
-        class="mb-3"
-      >
-        <label for="contact-email">
-          <icon name="bi:envelope-fill" /> Adresse e-mail
-        </label>
-        <b-form-input
-          id="contact-email"
-          v-model="email"
-          :disabled="formLoading"
-          placeholder="Entrez votre adresse e-mail"
-          aria-describedby="contact-email-help"
-          required
-        />
-      </b-col>
-      <b-col
-        cols="12"
-        class="mb-3"
-      >
-        <label for="contact-message">
-          <icon name="bi:pencil-fill" /> Message
-        </label>
-        <textarea
-          id="contact-message"
-          v-model="message"
-          class="form-control"
-          rows="5"
-          :disabled="formLoading"
-          placeholder="Entrez votre message"
-          aria-describedby="contact-message-help"
-          required
-        />
-      </b-col>
-      <b-col cols="12">
-        <b-button
-          id="contact-submit"
-          type="submit"
-          :disabled="formLoading || submitSuccess != null"
-          class="d-block w-100"
+  <div>
+    <h2>Contact</h2>
+    <b-form
+      action="https://formspree.io/xwpqolpp"
+      :method="method"
+      @submit.prevent="onSubmitContactForm"
+    >
+      <b-form-row>
+        <b-col
+          md="6"
+          sm="12"
+          class="mb-3"
         >
-          <icon name="bi:send-fill" /> Envoyer
-        </b-button>
-      </b-col>
-      <b-col
-        cols="12"
-        class="text-end"
-      >
-        <span
-          v-if="submitSuccess"
-          class="d-block mb-2 mb-md-0 text-success"
+          <label for="contact-name">
+            <icon name="bi:person-fill" /> Nom
+          </label>
+          <b-form-input
+            id="contact-name"
+            v-model="name"
+            :disabled="formLoading"
+            placeholder="Entrez votre nom"
+            aria-describedby="contact-name-help"
+            required
+          />
+        </b-col>
+        <b-col
+          md="6"
+          sm="12"
+          class="mb-3"
         >
-          <icon name="bi:check-lg" /> Votre message a été envoyé avec succès.
-        </span>
-        <span
-          v-if="submitError"
-          class="d-block mb-2 mb-md-0 text-danger"
+          <label for="contact-email">
+            <icon name="bi:envelope-fill" /> Adresse e-mail
+          </label>
+          <b-form-input
+            id="contact-email"
+            v-model="email"
+            :disabled="formLoading"
+            placeholder="Entrez votre adresse e-mail"
+            aria-describedby="contact-email-help"
+            required
+          />
+        </b-col>
+        <b-col
+          cols="12"
+          class="mb-3"
         >
-          <icon name="bi:exclamation-triangle-fill" /> Une erreur est survenue durant l'envoi de votre message.
-        </span>
-      </b-col>
-    </b-form-row>
-  </b-form>
+          <label for="contact-message">
+            <icon name="bi:pencil-fill" /> Message
+          </label>
+          <textarea
+            id="contact-message"
+            v-model="message"
+            class="form-control"
+            rows="5"
+            :disabled="formLoading"
+            placeholder="Entrez votre message"
+            aria-describedby="contact-message-help"
+            required
+          />
+        </b-col>
+        <b-col cols="12">
+          <b-button
+            id="contact-submit"
+            type="submit"
+            :disabled="formLoading || submitSuccess != null"
+            class="d-block w-100"
+          >
+            <icon name="bi:send-fill" /> Envoyer
+          </b-button>
+        </b-col>
+        <b-col
+          cols="12"
+          class="text-end"
+        >
+          <span
+            v-if="submitSuccess"
+            class="d-block mb-2 mb-md-0 text-success"
+          >
+            <icon name="bi:check-lg" /> Votre message a été envoyé avec succès.
+          </span>
+          <span
+            v-if="submitError"
+            class="d-block mb-2 mb-md-0 text-danger"
+          >
+            <icon name="bi:exclamation-triangle-fill" /> Une erreur est survenue durant l'envoi de votre message.
+          </span>
+        </b-col>
+      </b-form-row>
+    </b-form>
+  </div>
 </template>
